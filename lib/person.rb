7451ba21b1c7526aeed02038115dda5f5e0a3a06
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-16 18:03:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,8 +4,16 @@ class Person
     @name = persons_name
   end
   
+  def name
+    @name 
+  end 
+  
   def job=(persons_job)
     @job = persons_job
   end
+  
+  def job 
+    @job
+  end 
   
 end 
